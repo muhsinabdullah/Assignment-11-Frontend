6202f9f5189f8e33d1495c5924fc8b636470f493
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-12-14 23:29:04 +0600

    Role functionality add

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 import React, { createContext, useEffect, useState } from 'react';
 import auth from '../firebase/firebase.config';
+import axios from 'axios';
 
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -10,6 +11,8 @@ const AuthProvider = ({ children }) => {
 
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(null);
+    const [role, setRole] = useState('');
+
     const googleProvider = new GoogleAuthProvider();
 
     const registerWithEmailAndPassword = (email, pass) => {
@@ -19,17 +22,26 @@ const AuthProvider = ({ children }) => {
     const handleGoogleSignin = () => {
         return signInWithPopup(auth, googleProvider)
     }
+
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
 
             setUser(currentUser)
             setLoading(false)
-
         })
         return () => {
             unSubscribe();
         }
     }, [])
+
+
+    useEffect(() => {
+        if (!user) return;
+        axios.get(`http://localhost:5000/users/role/${user.email}`)
+            .then(res => {
+                setRole(res.data.role)
+            })
+    }, [user])
     const authData = {
         registerWithEmailAndPassword,
         setUser,
